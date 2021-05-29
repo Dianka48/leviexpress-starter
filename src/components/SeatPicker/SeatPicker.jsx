414d87dd3ab96c47baf1f48c4d0c3bd62af2b422
Diana Nemcekova <dianka.melegova@gmail.com>
2021-05-29 12:28:02 +0200

    reservation added

--- src/components/SeatPicker/SeatPicker.jsx
+++ src/components/SeatPicker/SeatPicker.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './style.css';
+import { useHistory } from 'react-router-dom';
 
 import SeatRow from '../SeatRow/SeatRow';
 
@@ -8,6 +9,24 @@ const SeatPicker = ({ seats, journeyId }) => {
 
   const handleSeatSelect = (number) => {
     setSelectedSeatNumber(number);
+  };
+
+  const history = useHistory();
+
+  const handleBuy = () => {
+    console.log('funguju');
+    fetch('https://leviexpress-backend.herokuapp.com/api/reserve', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        seat: selectedSeatNumber,
+        journeyId: journeyId,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => history.push(`/reservation/${data.data.reservationId}`));
   };
 
   return (
@@ -23,7 +42,12 @@ const SeatPicker = ({ seats, journeyId }) => {
           />
         ))}
       </div>
-      <button className="btn" type="button">
+      <button
+        onClick={handleBuy}
+        className="btn"
+        type="button"
+        disabled={selectedSeatNumber ? false : true}
+      >
         Rezervovat
       </button>
     </div>
