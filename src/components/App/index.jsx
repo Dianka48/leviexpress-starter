fc52e74cc5ce837d3645b91adaa855d5d8dcd9e1
Diana Nemcekova <dianka.melegova@gmail.com>
2021-05-29 10:16:00 +0200

    Reservation component added

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -2,11 +2,21 @@ import React from 'react';
 import { Footer } from '../Footer';
 import { Header } from '../Header';
 import { Home } from '../Home';
+import { Switch, Route } from 'react-router-dom';
+
+import Reservation from '../Reservation/Reservation';
 
 export const App = () => (
   <>
     <Header />
-    <Home />
+    <Switch>
+      <Route path="/" exact>
+        <Home />
+      </Route>
+      <Route path="/reservation" exact>
+        <Reservation />
+      </Route>
+    </Switch>
     <Footer />
   </>
 );
--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 import { App } from './components/App';
 import './style.css';
+import { BrowserRouter } from 'react-router-dom';
 
 export const API_BASE_URL = 'http://leviexpress-backend.herokuapp.com/api';
 
-render(<App />, document.querySelector('#app'));
+render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.querySelector('#app'),
+);
